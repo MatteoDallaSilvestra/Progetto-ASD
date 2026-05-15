dbf7151c7706f8f161b9e13598aa46daf60fe669
Edoardo <172879@spes.uniud.it>
2026-05-15 10:48:16 +0200

    DataStorage methods done

--- utils/DataStorage.py
+++ utils/DataStorage.py
@@ -1,3 +1,16 @@
 # Stores data in a dataframe and allows for easy access and manipulation of the data.
 import pandas as pd
 
+class DataStorage:
+    def __init__(self):
+        self._data = []
+    
+    def add_data(self, tree_type: str, key: int, insert_time: float):
+        self._data.append({
+            "Tree Type": tree_type,
+            "Key": key,
+            "Insert Time": insert_time
+        })
+
+    def get_dataframe(self):
+        return pd.DataFrame(self._data)
